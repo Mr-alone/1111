5747eb8d12d63f6f715530e27e996729feecc3c8
Mr-alone <s.a.moosavi1377@gmail.com>
2016-05-01 15:46:39 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {our_id},
+   sudo_users = {168757698,211952272},
     admin_users = {},
     disabled_channels = {}
   }
